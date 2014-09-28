8e2eecd023cff6c75785fa483385f426e4dcfcaa
louwrentius <louwrentius@gmail.com>
2014-09-29 00:40:49 +0200

    improved graphing and logging format

--- dylos.py
+++ dylos.py
@@ -7,7 +7,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=60)
 logfile = open('/dylos/test.csv', 'a')
 while True:
         line = ser.readline()
-        now = time.strftime("%Y-%m-%d,%H:%M:%S", time.localtime())
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         a =  "%s,%s" % (now,line)
         print a
         logfile.write(a)
--- plot.py
+++ plot.py
@@ -20,9 +20,14 @@ def plotter(xaxis, yaxis, xlabel, ylabel, title, filename):
         plt.plot_date(x=xaxis, y=yaxis, fmt="-")
 
 
-        ax1.xaxis.set_major_locator( mdates.HourLocator() )
-        ax1.xaxis.set_major_formatter( mdates.DateFormatter('%H'))
+        ax1.xaxis.set_major_locator( mdates.DayLocator() )
+        ax1.xaxis.set_major_formatter( mdates.DateFormatter('%Y-%m-%d'))
+        ax1.xaxis.set_minor_locator( mdates.HourLocator() )
+        ax1.xaxis.set_minor_formatter( mdates.DateFormatter('%H'))
 
+
+        xax1 = ax1.get_xaxis()
+        xax1.set_tick_params(which='major', pad=20)
 
 
 
@@ -36,12 +41,12 @@ def plotter(xaxis, yaxis, xlabel, ylabel, title, filename):
 
 def graph():
 	 data = np.loadtxt(inputfile, delimiter=',', unpack=True,
-                                       converters = {0: mdates.strpdate2num('%Y-%m-%d'), 1: mdates.strpdate2num('%H:%M:%S')}) 
+                                       converters = {0: mdates.strpdate2num('%Y-%m-%d %H:%M:%S')}) 
          return data
 
 
 
 data = graph()
 
-plotter(data[1],data[2],"time (24h)","Parts x 100 per cubic foot","Particle count below 2.5 micron","pm05.svg")
-plotter(data[1],data[3],"time (24h)","Parts x 100 per cubic foot","Particle count above 2.5 micron ","pm25.svg")
+plotter(data[0],data[1],"time (24h)","Parts x 100 per cubic foot","Particle count below 2.5 micron","pm05.svg")
+plotter(data[0],data[2],"time (24h)","Parts x 100 per cubic foot","Particle count above 2.5 micron ","pm25.svg")
