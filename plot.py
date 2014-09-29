55e0ad23209776440b17296383fe34dff8dd92b8
louwrentius <louwrentius@gmail.com>
2014-09-29 20:35:39 +0200

    added conversion to PM2.5 and PM10 standard

--- plot.py
+++ plot.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# coding: utf-8
+#
+# This code is horrible. 
+#
 import os
 import sys
 import datetime
@@ -6,18 +10,33 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.font_manager import FontProperties
 
 
 inputfile = sys.argv[1]
 
-def plotter(xaxis, yaxis, xlabel, ylabel, title, filename):
+def cubicmeters(array,factor):
+
+    temp = [x*3551.5*factor for x in array]
+    
+    return temp
 
 
-        fig = plt.figure()
+def plotter(xaxis, yaxis, xlabel, ylabel, title, filename, factor, limit1, limit2, convert):
+
+
+        if convert:
+            yaxis = cubicmeters(yaxis, factor)   
+
+        fig = plt.figure(figsize=(10,8))
         
         ax1 = fig.add_subplot(1,1,1, axisbg='white')
 
-        plt.plot_date(x=xaxis, y=yaxis, fmt="-")
+        plt.plot_date(x=xaxis, y=yaxis, fmt="-", label="Measurement")
+
+        if convert:
+            plt.axhline(y=limit1, linewidth=1,color='r',label="WHO AQG Limit")
+            plt.axhline(y=limit2, linewidth=1,color='c',label="European Limit 2015")
 
 
         ax1.xaxis.set_major_locator( mdates.DayLocator() )
@@ -28,25 +47,38 @@ def plotter(xaxis, yaxis, xlabel, ylabel, title, filename):
 
         xax1 = ax1.get_xaxis()
         xax1.set_tick_params(which='major', pad=20)
+        
+        ax1.set_ylim(0,)
+    
+        fontP = FontProperties()
+        fontP.set_size('small')
 
+        if convert:
+            ax1.legend(loc='lower center', fancybox=True, shadow=True, ncol=3, bbox_to_anchor=(0.5, -0.2), prop = fontP)
 
-
+        ax1.set_position([0.1,0.2,0.6,0.7])
+        
         plt.title(title)
         plt.ylabel(ylabel)
         plt.xlabel(xlabel)
         
-        plt.savefig(filename, format='svg')  
+        plt.savefig(filename, format='png')  
         
         
+    
+
 
 def graph():
 	 data = np.loadtxt(inputfile, delimiter=',', unpack=True,
                                        converters = {0: mdates.strpdate2num('%Y-%m-%d %H:%M:%S')}) 
+
          return data
 
 
 
 data = graph()
 
-plotter(data[0],data[1],"time (24h)","Parts x 100 per cubic foot","Particle count below 2.5 micron","pm05.svg")
-plotter(data[0],data[2],"time (24h)","Parts x 100 per cubic foot","Particle count above 2.5 micron ","pm25.svg")
+plotter(data[0],data[1],"time (24h)",u"Particles x 100 per cubic foot","PM2.5 Measurement","pm05.png",0.000000589,10,25,False)
+plotter(data[0],data[1],"time (24h)",u"µg per m$^3$ (aproximation)","PM2.5 Measurement","pm05-PM.png",0.000000589,10,25,True)
+plotter(data[0],data[2],"time (24h)",u"Particles x 100 per cubic foot","PM10 Measurement","pm25.png",0.000121,20,40,False)
+plotter(data[0],data[2],"time (24h)",u"µg per m$^3$ (aproximation)","PM10 Measurement","pm25-PM.png",0.000121,20,40,True)
